9fcf21d337ef5cd9d5e26eabf62ce1b7f684f88a
Halinston <ponomarenko.bodya@gmail.com>
2024-11-06 18:15:49 -0800

    changes in data form

--- display_results.php
+++ display_results.php
@@ -1,14 +1,16 @@
 <?php
     // get the data from the form
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_SPECIAL_CHARS);
+    $heard_from = filter_input(INPUT_POST, 'heard_from', FILTER_SANITIZE_SPECIAL_CHARS);
+    $heard_from = $heard_from ?: 'Unknown'; // default to 'Unknown' if not selected
+    $wants_updates = filter_input(INPUT_POST, 'wants_updates') ? 'Yes' : 'No';
+    $contact_via = filter_input(INPUT_POST, 'contact_via', FILTER_SANITIZE_SPECIAL_CHARS);
+    $comments = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    // get the rest of the data for the form
-
-    // for the heard_from radio buttons,
-    // display a value of 'Unknown' if the user doesn't select a radio button
-
-    // for the wants_updates check box,
-    // display a value of 'Yes' or 'No'
+    // Convert special characters in comments and handle new lines
+    $comments = nl2br(htmlspecialchars($comments));
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,22 +26,22 @@
         <span><?php echo htmlspecialchars($email); ?></span><br>
 
         <label>Password:</label>
-        <span><!-- add PHP code here--></span><br>
+        <span><?php echo str_repeat('*', strlen($password)); ?></span><br> <!-- Masking the password -->
 
         <label>Phone Number:</label>
-        <span></span><br>
+        <span><?php echo htmlspecialchars($phone); ?></span><br>
 
         <label>Heard From:</label>
-        <span></span><br>
+        <span><?php echo htmlspecialchars($heard_from); ?></span><br>
 
         <label>Send Updates:</label>
-        <span></span><br>
+        <span><?php echo htmlspecialchars($wants_updates); ?></span><br>
 
         <label>Contact Via:</label>
-        <span></span><br><br>
+        <span><?php echo htmlspecialchars($contact_via); ?></span><br><br>
 
         <span>Comments:</span><br>
-        <span></span><br>        
+        <span><?php echo $comments; ?></span><br>        
     </main>
 </body>
 </html>
